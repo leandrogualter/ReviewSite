1667cb326483a1ac4bded847c8f5577ef93aab00
Leandro Gualter <leandrogualter@gmail.com>
2014-07-07 15:11:12 -0300

    Poltergeist o/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,7 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
-  Capybara.javascript_driver = :poltergeist
+  Capybara.javascript_driver = :webkit
   config.include Capybara::DSL
 
   config.before(:suite) do
